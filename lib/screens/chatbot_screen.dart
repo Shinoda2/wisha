74229d2e1d61f7f116d83c43c72d82a20bd39e5d
Keiji Shinoda <k.shinoda.2000@hotmail.com>
2022-09-01 21:55:09 -0500

    Chatbot Heroku

---
+++ lib/screens/chatbot_screen.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class Chatbot_Screen extends StatefulWidget {
+  @override
+  _Chatbot_Screen createState() => _Chatbot_Screen();
+}
+
+class _Chatbot_Screen extends State<Chatbot_Screen> {
+  final GlobalKey<AnimatedListState> _listKey = GlobalKey();
+  List<String> _data = [];
+  /*static const String BOT_URL = ""*/
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
